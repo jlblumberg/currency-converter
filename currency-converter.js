9d82c782cb6c287f2c23380e186a14dc22905f87
Josh Blumberg <jlblumberg@gmail.com>
2020-06-24 12:20:24 +0100

    Add convertCurrency to do the combined work

--- currency-converter.js
+++ currency-converter.js
@@ -4,7 +4,7 @@ const currencyUrl = 'http://data.fixer.io/api/latest?access_key=f68b13604ac8e570
 const countryUrl = 'https://restcountries.eu/rest/v2/currency'
 
 const getExchangeRate = async (fromCurrency, toCurrency) => {
-  const response = await axios.get(url);
+  const response = await axios.get(currencyUrl);
 
   const rates = response.data.rates;
   const base = 1 / rates[fromCurrency];
@@ -18,5 +18,15 @@ const getCountries = async (toCurrency) => {
   return response.data.map(country => country.name);
 }
 
-// getExchangeRate('CAD', 'GBP');
-// getCountries('USD');
+const convertCurrency = async (fromCurrency, toCurrency, amount) => {
+  const exchangeRate = await getExchangeRate(fromCurrency, toCurrency);
+  const countries = await getCountries(toCurrency);
+  const convertedAmount = (amount * exchangeRate).toFixed(2);
+
+  return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries}`
+}
+
+convertCurrency('USD', 'HRK', 30)
+  .then((message) => {
+    console.log(message);
+  });
