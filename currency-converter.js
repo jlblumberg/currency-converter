4ebf512ae6cff5e2d3d5feab354e88b3464616cb
Josh Blumberg <jlblumberg@gmail.com>
2020-06-24 11:54:48 +0100

    Update exchangeRate to use async/await

--- currency-converter.js
+++ currency-converter.js
@@ -2,15 +2,14 @@ const axios = require('axios');
 
 const url = 'http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1'
 
-const getExchangeRate = (fromCurrency, toCurrency) => {
-  axios.get(url)
-    .then((response) => {
-      const rates = response.data.rates
-      const base = 1 / rates[fromCurrency];
-      const exchangeRate = base * rates[toCurrency];
-      console.log(exchangeRate);
-    });
+const getExchangeRate = async (fromCurrency, toCurrency) => {
+  const response = await axios.get(url);
 
+  const rates = response.data.rates;
+  const base = 1 / rates[fromCurrency];
+  const exchangeRate = base * rates[toCurrency];
+  
+  return exchangeRate;
 }
 
 getExchangeRate('CAD', 'GBP');
