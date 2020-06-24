403a3e8fe389885125f525cec3d501dbc8fb92f7
Josh Blumberg <jlblumberg@gmail.com>
2020-06-24 12:07:31 +0100

    Add getCountries, to return countries where currency can be used

--- currency-converter.js
+++ currency-converter.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
-const url = 'http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1'
+const currencyUrl = 'http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1'
+const countryUrl = 'https://restcountries.eu/rest/v2/currency'
 
 const getExchangeRate = async (fromCurrency, toCurrency) => {
   const response = await axios.get(url);
@@ -8,8 +9,14 @@ const getExchangeRate = async (fromCurrency, toCurrency) => {
   const rates = response.data.rates;
   const base = 1 / rates[fromCurrency];
   const exchangeRate = base * rates[toCurrency];
-  
+
   return exchangeRate;
 }
 
-getExchangeRate('CAD', 'GBP');
+const getCountries = async (toCurrency) => {
+  const response = await axios.get(`${countryUrl}/${toCurrency}`)
+  return response.data.map(country => country.name);
+}
+
+// getExchangeRate('CAD', 'GBP');
+// getCountries('USD');
